4b9721aefe52ccdd0e1d44ea412972496b29602a
dongwenbo <2668287186@qq.com>
2020-05-25 12:05:10 +0800

    flutter`

---
+++ flutter_demo_1/lib/pages/tabs/HomePage1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("首页"),
+      ),
+    );
+  }
+}
